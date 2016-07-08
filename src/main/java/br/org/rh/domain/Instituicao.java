8b6a695558ccbe04f0bcf492ee583cf26b9ddb5a
Leonardo Ribeiro Silva <Leonardo Ribeiro Silva>
2016-07-08 19:18:59 -0300

    Atualização da tabela instituicao

--- src/main/java/br/org/rh/domain/Instituicao.java
+++ src/main/java/br/org/rh/domain/Instituicao.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 @Table(name = "instituicao")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 @Document(indexName = "instituicao")
-public class Instituicao implements Serializable {
+public class Instituicao extends AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
