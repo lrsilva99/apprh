65ad0eda855f7d341ad13500e74d4a908a6721cc
Leonardo Ribeiro Silva <Leonardo Ribeiro Silva>
2016-07-09 12:06:57 -0300

    Adicionado log de alteracao

---
+++ src/main/java/br/org/rh/config/audit/EntityAuditEventConfig.java
@@ -0,0 +1,14 @@
+package br.org.rh.config.audit;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EntityAuditEventConfig implements BeanFactoryAware {
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        EntityAuditEventListener.setBeanFactory(beanFactory);
+    }
+}
